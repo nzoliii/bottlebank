3659b72d1dfa004b1eb730917866f12a5f86e6cf
nzoliii <nemethzoltanjozsef@gmail.com>
2024-10-20 23:34:29 +0200

    ezt nem hiszem el de kész

--- app/src/main/java/com/peroni/bottlebank/MainActivity.kt
+++ app/src/main/java/com/peroni/bottlebank/MainActivity.kt
@@ -13,72 +13,72 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.activity_main)
 
         // Alap adatbázis.
-        data class uveg(
-            val nev: String,
-            val terfogat: Int,
-            val ertek: Int
+        data class bottle(
+            val name: String,
+            val volume: Int,
+            val value: Int
         )
 
-        // UI-ból adatok lekérése.
+        // UI-ból adatok lekérése
         val savingsInput = findViewById<EditText>(R.id.SavingsInput_editTextNumber)
         val volumeInput = findViewById<EditText>(R.id.VolumeInput_editTextNumber)
         val resultText = findViewById<TextView>(R.id.Result_TextView)
         val calculateButton = findViewById<Button>(R.id.CalculateButton_Button)
 
-        // Üvegek listája.
-        val uvegek = listOf(
-            uveg("Sörösüveg", 500, 60),
-            uveg("0,5 literes műanyag", 500, 50),
-            uveg("2 literes műanyag", 2000, 80)
+        // Üvegek listája
+        val bottles = listOf(
+            bottle("Sörösüveg", 500, 60),
+            bottle("0,5 literes műanyag", 500, 50),
+            bottle("2 literes műanyag", 2000, 80)
         )
 
-        // Számitás lefuttatása a gomb lenyomásakor.
+        // Számitás lefuttatása a gomb lenyomásakor
         calculateButton.setOnClickListener {
-            val megtakaritas = savingsInput.text.toString().toIntOrNull() ?: 0
-            val csomagterTerfogat = volumeInput.text.toString().toIntOrNull() ?: 0
+            val savings = savingsInput.text.toString().toIntOrNull() ?: 0
+            val storageVolume = volumeInput.text.toString().toIntOrNull() ?: 0
 
-            if (megtakaritas <= 0 || csomagterTerfogat <= 0) {
+            if (savings <= 0 || storageVolume <= 0) {
                 resultText.text = "Hibás adat! A megtakarítás és a csomagtartó térfogatának pozitív számnak kell lennie."
                 return@setOnClickListener
             }
 
-            // Számítás
-            var osszTerfogat = 0
-            var osszErtek = 0
-            val uvegDarabok = mutableListOf<Pair<String, Int>>()
+            // Számítás folyamata
+            var totalVolume = 0
+            var totalValue = 0
+            val pieceofBottles = mutableListOf<Pair<String, Int>>()
 
-            for (uveg in uvegek) {
-                val darabszam = megtakaritas / uveg.ertek
-                osszErtek += darabszam * uveg.ertek
-                uvegDarabok.add(Pair(uveg.nev, darabszam))
-                osszTerfogat += darabszam * uveg.terfogat
+            for (bottle in bottles) {
+                val pieces = savings / bottle.value
+                totalValue += pieces * bottle.value
+                pieceofBottles.add(Pair(bottle.name, pieces))
+                totalVolume += pieces * bottle.volume
             }
 
             // Szükséges fordulók kiszámítása
-            val eredmeny = StringBuilder()
+            val result = StringBuilder()
 
-            if (osszErtek >= megtakaritas) {
-                if (osszTerfogat <= csomagterTerfogat) {
-                    eredmeny.append("Egy forduló elég lesz. Összesen $osszTerfogat cm³ térfogatot foglalnak az üvegek.\n")
+            if (totalValue >= savings) {
+                if (totalVolume <= storageVolume) {
+                    result.append("Egy forduló elég lesz. Összesen $totalVolume cm³ térfogatot foglalnak az üvegek.\n")
                 }
 
                 else {
-                    val fordulok = ceil(osszTerfogat.toDouble() / csomagterTerfogat).toInt()
-                    eredmeny.append("$fordulok fordulóra lesz szükséged.\n")
+                    val turns = ceil(totalVolume.toDouble() / storageVolume).toInt()
+                    result.append("$turns fordulóra lesz szükséged.\n")
                 }
 
-                eredmeny.append("Az üvegek elosztása:\n")
-                for ((nev, darab) in uvegDarabok) {
-                    eredmeny.append("$darab darab $nev\n")
+                result.append("Az üvegek elosztása:\n")
+                for ((name, pieces) in pieceofBottles) {
+                    result.append("$pieces darab $name\n")
                 }
             }
 
             else {
-                eredmeny.append("Nincs elég üveg az adott megtakarítás eléréséhez.")
+                result.append("Nincs elég üveg az adott megtakarítás eléréséhez.")
             }
 
             // Eredmény megjelítése
-            resultText.text = eredmeny.toString()
+            resultText.text = result.toString()
         }
     }
 }
