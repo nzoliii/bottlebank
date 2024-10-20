d2e9ad1344079cea7763093bd9f346d8bc122d7f
nzoliii <nemethzoltanjozsef@gmail.com>
2024-10-20 21:39:05 +0200

    ez a feladat biztos nem lesz kesz

--- app/src/main/java/com/nzoliii/bottlebank/MainActivity.kt
+++ app/src/main/java/com/nzoliii/bottlebank/MainActivity.kt
@@ -2,38 +2,23 @@ package com.nzoliii.bottlebank
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.nzoliii.bottlebank.ui.theme.ÜvegBankTheme
+import com.nzoliii.bottlebank.ui.theme.BottleBankTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            ÜvegBankTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "help me",
         modifier = modifier
     )
 }
@@ -41,7 +26,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ÜvegBankTheme {
+    BottleBankTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/nzoliii/bottlebank/ui/theme/Theme.kt
+++ app/src/main/java/com/nzoliii/bottlebank/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ÜvegBankTheme(
+fun BottleBankTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
